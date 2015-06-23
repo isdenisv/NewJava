f840d815e2f7e43724963a5e33c9dd833ef1d2d8
isdenisv <isdenisv@mail.ru>
2015-06-23 17:38:10 +0300

    commit

--- XO/src/helpers/Helper.java
+++ XO/src/helpers/Helper.java
@@ -6,10 +6,10 @@ import view.ConsoleView;
 public class Helper {
 
     public static boolean checkXCoordinate(int x) {
-        if (x < 0 || x >= GameBoard.NUMBER_OF_CELL) {
+        if (x < 0 || x >= GameBoard.NUMBER_OF_CELL) [{
             ConsoleView.sayAboutError();
             return false;
-        }
+        }]
         return true;
     }
 
